5b9e3978a062d08dbd0bacdbfe52f6d4112a1636
930419 <118976064+930419@users.noreply.github.com>
2026-02-25 01:58:41 +0800

    update

--- bot.py
+++ bot.py
@@ -5,11 +5,9 @@ intents.messages = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-# 從 JSON 載入集成資料
+# 載入 JSON
 with open("jiname.json", "r", encoding="utf-8") as f:
     ji = json.load(f)
-
-# 從 JSON 載入六星
 with open("sixstar.json", "r", encoding="utf-8") as f:
     sixstar = json.load(f)
 
@@ -24,18 +22,22 @@ async def on_message(message):
 
     content = message.content.strip()
 
-    # 判斷訊息開頭是 "集"，且有標註機器人
     if "集" in content and client.user in message.mentions:
-        # 先抽外層主題
+        # 抽主題
         theme = random.choice(list(ji.keys()))
-        # 再抽內層分隊
-        team = random.choice(ji[theme])
-        # 抽開局六星
-        selected_sixstar = random.choice(sixstar)
+        # 抽分隊
+        team = random.choice(ji[theme]["分隊"])
         # 抽結局
-        selected_out = random.randint(1,5)
+        ending = random.choice(ji[theme]["結局"])
+        # 抽六星
+        selected_sixstar = random.choice(sixstar)
+
         await message.channel.send(
-            f"抽取結果：\n{theme}\n{team}\n{selected_sixstar}開\n{selected_out}結局"
+            f"抽取結果：\n"
+            f"{theme}\n"
+            f"{team}\n"
+            f"{selected_sixstar}開\n"
+            f"{ending}"
         )
 
 try:
